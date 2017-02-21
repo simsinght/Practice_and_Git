2ba7c6a96235bcbc56e3891ad6fe19c2a9162796
simsinght <simsinght@gmail.com>
2017-02-20 23:20:23 -0800

    YELLp

---
+++ CodePath/lab3/GithubDemo/RepoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoTableViewCell.swift
+//  GithubDemo
+//
+//  Created by Simrandeep Singh on 2/16/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
